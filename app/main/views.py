723338a673271d33b2f423297d2368770775531e
ghosthole <rasteraevvlad@gmail.com>
2024-11-13 18:09:15 +0300

    The main app is ready, pages index and about

--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина'
+        'title': 'HOME - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'HOME - О нас',
+        'content': 'Страница о нас',
+        'text_on_page': 'Какой-то текст'
+    }
+
+    return render(request, 'main/about.html', context)
